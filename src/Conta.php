b574f737ba3f6862241b4788f79c337d5ed07a33
Elias Moraes <eeliasmaia@gmail.com>
2021-03-23 13:27:08 -0300

    Protegendo os nossos dados

--- src/Conta.php
+++ src/Conta.php
@@ -2,40 +2,63 @@
 
 class Conta
 {
-    public $cpfTitular;
-    public $nomeTitular;
-    public $saldo = 0;
+    private  $cpfTitular;
+    private  $nomeTitular;
+    private  $saldo = 0;
 
-    public function sacar(float $valorASacar): void
+    public function saca(float $valorASacar): void
     {
-        if($valorASacar > $this->saldo){
+        if ($valorASacar > $this->saldo) {
             echo "Saldo indisponível";
             return;
-        } 
-            
+        }
+
         $this->saldo -= $valorASacar;
     }
 
-    public function depositar(float $valorADepositar): void
+    public function deposita(float $valorADepositar): void
     {
-        if($valorADepositar < 0){
+        if ($valorADepositar < 0) {
             echo "Valor precisa ser positivo";
             return;
         }
 
         $this->saldo += $valorADepositar;
-        
     }
 
-    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
     {
-        if($valorATransferir > $this->saldo){
+        if ($valorATransferir > $this->saldo) {
             echo "Saldo indisponível";
             return;
         }
 
-        $this->sacar($valorATransferir);
-        $contaDestino->depositar($valorATransferir);
-        
+        $this->saca($valorATransferir);
+        $contaDestino->deposita($valorATransferir);
+    }
+
+    public function getSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function setCpfTitular(string $cpf): void
+    {
+        $this->cpfTitular = $cpf;
+    }
+
+    public function getCpfTitular(): string
+    {
+        return $this->cpfTitular;
+    }
+
+    public function setNomeTitular(string $nome): void
+    {
+        $this->nomeTitular = $nome;
+    }
+
+    public function getNomeTitular(): string
+    {
+        return $this->nomeTitular;
     }
 }

a06bee0c11e87c721aec460ce40e8eb06e48d2d6
Elias Moraes <eeliasmaia@gmail.com>
2021-03-23 11:41:34 -0300

    Definindo os comportamentos

--- src/Conta.php
+++ src/Conta.php
@@ -4,5 +4,38 @@ class Conta
 {
     public $cpfTitular;
     public $nomeTitular;
-    public $saldo;
+    public $saldo = 0;
+
+    public function sacar(float $valorASacar): void
+    {
+        if($valorASacar > $this->saldo){
+            echo "Saldo indisponível";
+            return;
+        } 
+            
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if($valorADepositar < 0){
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+        
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if($valorATransferir > $this->saldo){
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+        
+    }
 }
